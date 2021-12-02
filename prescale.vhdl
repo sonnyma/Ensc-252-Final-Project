30b460270c59e988a497fb52ceeb965dda95f2fb
Brian Chan <brianchan418@gmail.com>
2021-12-02 15:39:30 -0800

    PreScale edit (clock speed is not correct)

--- prescale.vhdl
+++ prescale.vhdl
@@ -1,28 +1,34 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
 
-entity prescale is
-    port (
-        clk : in std_logic;
-        mode : in std_logic_vector(1 downto 0);
-        clk_out : out std_logic);
-    );
-end prescale;
+entity PreScale is
+    port (clk   : in std_logic;
+	    mode	: in std_logic_vector(1 downto 0);
+		clk_out   : out std_logic);
+end PreScale;
 
-architecture rtl of prescale is
+architecture arch of PreScale is
+
+signal count : std_logic_vector(19 downto 0) := (others => '0');
 
 begin
 
     process (clk,mode)
     begin
-        if (rising_edge (clk) and mode = '01') then
+        if rising_edge (clk) and mode = "01" then -- normal speed
             count <= count + 1;
         end if;
-        if (rising_edge (clk) and mode = '10') then
+		if rising_edge (clk) and mode = "01" then -- slow speed
             count <= count + 2;
         end if;
-    clk_out <= count;
+		if rising_edge (clk) and mode = "10" then -- fast speed
+            count <= count + 3;
+        end if;
+		if rising_edge (clk) and mode = "11" then -- very fast speed
+            count <= count + 4;
+        end if;
+    clk_out <= count (19);
     end process;
 
-end architecture;
+end arch;
