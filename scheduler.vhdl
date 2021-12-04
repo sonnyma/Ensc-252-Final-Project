f4074eb456da485c60a20e46ff62aca207612a8e
Brian Chan <brianchan418@gmail.com>
2021-12-03 18:44:12 -0800

    Scheduler cleaned up

--- scheduler.vhdl
+++ scheduler.vhdl
@@ -4,33 +4,23 @@ use ieee.numeric_std.all;
 
 entity scheduler is
     port (
-        clk : in std_logic;
-        SW : in std_logic_vector(17 downto 0);	
-        KEY : in std_logic_vector(3 downto 2);
+        clk, rst, hard_rst, stop_prog : in std_logic;
+        program : in std_logic_vector(3 downto 0);
         pce : out std_logic;
-        inst_out : out std_logic_vector(something downto something)
+        inst_out : out std_logic_vector(2 downto 0)
         );
 end scheduler;
 
 architecture behaviour of scheduler is
-
-signal program : std_logic_vector(1 downto 0);
-signal stop_prog :std_logic;
-signal speed : std_logic_vector(1 downto 0);
-signal rst : std_logic;
-
 begin
-    program <= SW(3 downto 0);
-    speed <= SW(6 downto 4);
-    rst <= KEY(3);
-    stop_prog <= KEY(2);
     process(clk)
     begin
         if rising_edge(clk) then
             if stop_prog = '1' then
                 pce <= '1';
+                --terminate program (return to idle)
             elsif rst = '1' then
-                --pause
+                --pause (set count = )
             else
                 pce <= '0';
                 if program = "0001" then
@@ -51,5 +41,4 @@ begin
                 end if;
             end if;
         end if;
-            
 end behaviour;
